00142ceb5d2fc4deedffbd65ddbb52e483c1f5ce
Darshan <dhabaledarshan@gmail.com>
2020-12-18 13:13:04 +0530

    added gud landing page

--- twilio_video/static/js/create_room.js
+++ twilio_video/static/js/create_room.js
@@ -1,15 +1,16 @@
 const create_room = async() =>{
-    let btn = document.querySelector('#create_room_btn') 
+    let btn = document.querySelector('#create_room_btn')  
     btn.remove()    
     let room_data = await fetch('/create_room')
     let room_data_json = await room_data.json();
     sessionStorage.setItem('room_name',room_data_json.room_name)
     sessionStorage.setItem('room_sid',room_data_json.room_sid)
     let room_link = document.createElement('a')
+    const container = document.querySelector('.container')
     room_link.href = room_data_json.room_name
     room_link.textContent = "Join Room"
     room_link.setAttribute('id', 'join_room_btn')
-    document.body.appendChild(room_link)
+    container.appendChild(room_link)
     let date = new Date();
     let room_start_time = date.getTime();
     sessionStorage.setItem('room_start_time', room_start_time)
